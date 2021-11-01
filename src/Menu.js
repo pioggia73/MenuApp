27238ddbca9e6cca83cabb877ed8763826333c9b
Monika Wasilewska <monikawasilewska13@wp.pl>
2021-11-01 19:38:25 +0100

    changes

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Menu = ({items}) => {
   return  <div className="section-center">
             {items.map((menuItem) => {
-              const {id, title, category, price, img, desc} = menuItem;
+              const {id, title, price, img, desc} = menuItem;
               return  <article key={id} className="menu-item">
                         <img src={img} alt={title} className="photo"/>
                         <div className="item-info">
